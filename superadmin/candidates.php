ec9d207e4c1f6a26ec2c387efd54e9899ef50996
itsmeralph09 <ralphcustodio@pcb.edu.ph>
2024-09-18 00:14:10 +0800

    Added add function for candidate

--- superadmin/candidates.php
+++ superadmin/candidates.php
@@ -129,7 +129,7 @@
                                 </div>
                             </div>
                         </div>
-                        <?php //include('modal/candidate_add_modal.php'); ?>
+                        <?php include('modal/candidate_add_modal.php'); ?>
                     </div>
                     
                 </div>
@@ -173,8 +173,53 @@
           'fadeDuration': 400
         })
     </script>
+    <script>
+         // Birth Date Validation
+         document.getElementById('birthdate').addEventListener('change', function() {
+             var inputDate = new Date(this.value);
+             var currentDate = new Date();
 
-    <!-- Delete User Account -->
+             // Check if the input date is set and not in the future
+             if (!this.value || inputDate > currentDate) {
+                 Swal.fire({
+                     icon: 'error',
+                     title: 'Invalid...',
+                     text: 'Please select a valid birthdate.'
+                 });
+                 $('input[name="birthdate"]').addClass('is-invalid');
+                 this.value = ''; // Clear the input field
+             } else if (inputDate.getMonth() === currentDate.getMonth() && inputDate.getDate() === currentDate.getDate() && inputDate.getFullYear() === currentDate.getFullYear()) {
+                 Swal.fire({
+                     icon: 'error',
+                     title: 'Invalid...',
+                     text: 'Please select a valid birthdate.'
+                 });
+                 $('input[name="birthdate"]').addClass('is-invalid');
+                 this.value = ''; // Clear the input field
+             }
+         });
+
+         // Input Element for Contact Number
+         function limitContactInputLength(event) {
+             // Remove non-digit characters
+             var inputValue = event.target.value.replace(/\D/g, '');
+
+             // Limit the length to 11 digits
+             if (inputValue.length > 11) {
+                 inputValue = inputValue.slice(0, 11);
+             }
+
+             // Update the input value
+             event.target.value = inputValue;
+         }
+
+         // Contact Input Validation
+         var contactInput = document.getElementById('contact_number');
+         contactInput.addEventListener('input', limitContactInputLength);
+
+      </script>
+
+    <!-- Delete Candidate Account -->
     <script>
         $(document).ready(function() {
             // Function for deleting event
@@ -236,9 +281,13 @@
             });
         });
     </script>
-    <!-- Add User Account-->
+
+    <!-- Add Candidate Account-->
     <script>
         $(document).ready(function() {
+            // Variable to track if the profile picture is changed
+            let profileValid = false;
+
             // Function to show SweetAlert2 warning message
             const showWarningMessage = (message) => {
                 Swal.fire({
@@ -248,44 +297,43 @@
                 });
             };
 
-            // Function to check if username or email already exists
-            const checkExistingUser = (formData) => {
-                return new Promise((resolve, reject) => {
-                    $.ajax({
-                        url: 'action/check_user_exists.php', // URL to check the database
-                        type: 'POST',
-                        data: formData.serialize(), // Serialize form data
-                        success: function(response) {
-                            if (response.exists) {
-                                // Highlight the corresponding input fields with red border
-                                if (response.exists.username && response.exists.email) {
-                                    showWarningMessage('Username and Email already exists.');
-                                    formData.find('input[name="username"]').addClass('input-error');
-                                    formData.find('input[name="email"]').addClass('input-error');
-                                } else if (response.exists.username) {
-                                    showWarningMessage('Username already exists.');
-                                    formData.find('input[name="username"]').addClass('input-error');
-                                } else if (response.exists.email) {
-                                    showWarningMessage('Email already exists.');
-                                    formData.find('input[name="email"]').addClass('input-error');
-                                }
-                                reject(); // Reject the promise if department already exists
-                            } else {
-                                resolve(); // Resolve the promise if department doesn't exist
-                            }
-                        },
-                        error: function(xhr, status, error) {
-                            console.error(xhr.responseText); // Output error response to console (for debugging)
-                            reject(); // Reject the promise if there's an error
-                        }
-                    });
-                });
-            };
+            // Function to handle file input change event for profile picture
+            $('#profileUpload').on('change', function() {
+                const fileInput = $(this)[0];
+                const file = fileInput.files[0];
 
-            $('#addUser').on('click', function(e) {
+                // Update the label text with the selected file name
+                $(this).next('#profileLabel').text(file.name);
+
+                // Set profileValid to true when a new profile picture is selected
+                profileValid = true;
+
+                // Check if the file type is allowed
+                const allowedTypes = ['image/png', 'image/jpeg', 'image/webp'];
+                if (allowedTypes.includes(file.type)) {
+                    // Read the selected file and display the preview
+                    const reader = new FileReader();
+                    reader.onload = function(e) {
+                        $('#profilePreview').attr('src', e.target.result); // Set image source to preview element
+                        $('input[name="profile"]').removeClass('input-error');
+                        $('.custom-file-label[for="profileUpload"]').removeClass('input-error'); // Add input-error class to the label as well
+                    };
+                    reader.readAsDataURL(file);
+                } else {
+                    // Show warning message for invalid file type
+                    showWarningMessage('Please select a valid image file (PNG, JPG, WEBP).');
+                    profileValid = false;
+                    $('#profileUpload').val(''); // Clear the file input
+                    $('input[name="profile"]').addClass('input-error');
+                    $('.custom-file-label[for="profileUpload"]').addClass('input-error'); // Add input-error class to the label as well
+                }
+            });
+
+            $('#addCandidate').on('click', function(e) {
                 e.preventDefault(); // Prevent default form submission
 
                 var formData = $('#addNew form'); // Select the form element
+                var formDataSend = new FormData($('#addNew form')[0]); // Select the form element
 
                 const requiredFields = formData.find('[required], select');
                 let fieldsAreValid = true; // Initialize as false
@@ -325,58 +373,65 @@
                 }
 
                 if (fieldsAreValid && passwordsAreValid) {
-                    checkExistingUser(formData).then(() => {
-                        // If username or email doesn't exist, proceed with form submission
-                        $.ajax({
-                            url: 'action/add_user.php', // URL to submit the form data
-                            type: 'POST',
-                            data: formData.serialize(), // Serialize form data
-                            success: function(response) {
-                                // Handle the success response
-                                console.log(response); // Output response to console (for debugging)
-                                if (response === 'success') {
-                                    Swal.fire({
-                                        icon: 'success',
-                                        title: 'User added successfully!',
-                                        showConfirmButton: true, // Show OK button
-                                        confirmButtonText: 'OK'
-                                    }).then(() => {
-                                        location.reload();
-                                    });
-                                } else {
-                                    Swal.fire({
-                                        icon: 'error',
-                                        title: 'Failed to add user!',
-                                        text: 'Please try again later.',
-                                        showConfirmButton: true,
-                                        confirmButtonText: 'OK'
-                                    }).then(() => {
-                                        location.reload();
-                                    });
-                                }
-                            },
-                            error: function(xhr, status, error) {
-                                // Handle the error response
-                                console.error(xhr.responseText); // Output error response to console (for debugging)
+                    // Check if profile picture is changed
+                    if (!profileValid) {
+                        showWarningMessage('Please upload a valid picture.');
+                        $('input[name="profile"]').addClass('input-error');
+                        $('.custom-file-label[for="profileUpload"]').addClass('input-error'); // Add input-error class to the label as well
+                        return; // Stop form submission
+                    }
+                        
+                    $.ajax({
+                        url: 'action/add_candidate.php', // URL to submit the form data
+                        type: 'POST',
+                        data: formDataSend,
+                        contentType: false, // Important: Prevent jQuery from setting contentType
+                        processData: false,
+                        success: function(response) {
+                            // Handle the success response
+                            console.log(response); // Output response to console (for debugging)
+                            if (response === 'success') {
                                 Swal.fire({
-                                    icon: 'error',
-                                    title: 'Failed to add user',
-                                    text: 'Please try again later.',
+                                    icon: 'success',
+                                    title: 'Candidate added successfully!',
                                     showConfirmButton: true, // Show OK button
                                     confirmButtonText: 'OK'
                                 }).then(() => {
                                     location.reload();
                                 });
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Failed to add candidate!',
+                                    text: 'Please try again later.',
+                                    showConfirmButton: true,
+                                    confirmButtonText: 'OK'
+                                }).then(() => {
+                                    location.reload();
+                                });
                             }
-                        });
-                     }).catch(() => {
-                        // If user exists, do nothing (error message already shown)
+                        },
+                        error: function(xhr, status, error) {
+                            // Handle the error response
+                            console.error(xhr.responseText); // Output error response to console (for debugging)
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Failed to add candidate',
+                                text: 'Please try again later.',
+                                showConfirmButton: true, // Show OK button
+                                confirmButtonText: 'OK'
+                            }).then(() => {
+                                location.reload();
+                            });
+                        }
                     });
+                     
                 }
             });
         });
     </script>
-    <!-- Update User Account -->
+
+    <!-- Update Candidate Account -->
     <script>
         $(document).ready(function() {
             // Function to show SweetAlert2 messages
